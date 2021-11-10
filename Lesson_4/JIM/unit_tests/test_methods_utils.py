35edbfae72b6347cddfc38a9b4002a251341971f
ipavka <ipavka@ya.ru>
2021-11-10 14:51:56 +0300

    fixed lesson_4

--- Lesson_4/JIM/unit_tests/test_methods_utils.py
+++ Lesson_4/JIM/unit_tests/test_methods_utils.py
@@ -12,8 +12,7 @@ class TestsMethodsUtils(unittest.TestCase):
     """ Класс для тестирования общих методов"""
 
     def test_get_ip_and_port(self):
-        self.assertEqual(get_ip_and_port(), sys.argv[1],
-                         (DEFAULT_IP_ADDRESS, DEFAULT_PORT))
+        self.assertEqual(get_ip_and_port(), (DEFAULT_IP_ADDRESS, DEFAULT_PORT))
 
 
 if __name__ == '__main__':
